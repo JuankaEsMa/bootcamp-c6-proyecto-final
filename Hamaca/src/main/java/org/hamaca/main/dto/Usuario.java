70f9846ddbd86940d78cadb4846b630721bda0ea
JuankaEsMa <joancarles.escobar@gmail.com>
2023-11-22 17:04:16 +0100

    first commit

---
+++ Hamaca/src/main/java/org/hamaca/main/dto/Usuario.java
@@ -0,0 +1,7 @@
+package org.hamaca.main.dto;
+
+public class Usuario {
+	private int id;
+	private String nombre;
+	
+}
---
+++ Hamaca/src/test/java/org/hamaca/main/HamacaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hamaca.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
