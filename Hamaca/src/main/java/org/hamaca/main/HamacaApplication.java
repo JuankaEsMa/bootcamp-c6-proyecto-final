3bb3fd254567fe82245089b3df26af4b7052e07b
JuankaEsMa <joancarles.escobar@gmail.com>
2023-11-22 17:10:50 +0100

    subir proyecto inicial

---
+++ Hamaca/src/main/java/org/hamaca/main/HamacaApplication.java
@@ -0,0 +1,13 @@
+package org.hamaca.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamacaApplication.class, args);
+	}
+
+}
