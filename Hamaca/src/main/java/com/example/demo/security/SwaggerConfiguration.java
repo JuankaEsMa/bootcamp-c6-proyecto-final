33a8682e8a660de97f0e6d7e362b159784980fb3
JuankaEsMa <joancarles.escobar@gmail.com>
2023-12-14 14:20:31 +0100

    cambio ruta swagger 2

--- Hamaca/src/main/java/com/example/demo/security/SwaggerConfiguration.java
+++ Hamaca/src/main/java/com/example/demo/security/SwaggerConfiguration.java
@@ -1,7 +1,13 @@
 package com.example.demo.security;
 
+import java.util.Properties;
+
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,7 +17,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements ApplicationListener<ApplicationPreparedEvent>{
 
     @Bean
     public OpenAPI openAPI() {
@@ -29,4 +35,13 @@ public class SwaggerConfiguration {
             .scheme("bearer")
             .description("Authorize Client");
     }
+	@Override
+	public void onApplicationEvent(ApplicationPreparedEvent event) {
+		ConfigurableEnvironment environment = event.getApplicationContext().getEnvironment();
+        Properties props = new Properties();
+        props.put("springdoc.swagger-ui.path", "/");
+        environment.getPropertySources()
+          .addFirst(new PropertiesPropertySource("programmatically", props));
+	}
+		
 }
